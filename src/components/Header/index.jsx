fc56ad09bbf8f5656ef21d2f74116e810dab63c6
everethdev <tatecashtoken@gmail.com>
2025-08-06 17:40:22 +0300

    first commit burger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,13 @@
 'use client';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './style.module.scss';
 import { usePathname } from 'next/navigation';
 import { AnimatePresence } from 'framer-motion';
 import Nav from './nav';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Rounded from '../../common/RoundedButton';
 import Magnetic from '../../common/Magnetic';
 
 export default function index() {
-    const header = useRef(null);
     const [isActive, setIsActive] = useState(false);
     const pathname = usePathname();
     const button = useRef(null);
@@ -18,19 +15,6 @@ export default function index() {
     useEffect( () => {
       if(isActive) setIsActive(false)
     }, [pathname])
-
-    useLayoutEffect( () => {
-        gsap.registerPlugin(ScrollTrigger)
-        gsap.to(button.current, {
-            scrollTrigger: {
-                trigger: document.documentElement,
-                start: 0,
-                end: window.innerHeight,
-                onLeave: () => {gsap.to(button.current, {scale: 1, duration: 0.25, ease: "power1.out"})},
-                onEnterBack: () => {gsap.to(button.current, {scale: 0, duration: 0.25, ease: "power1.out"},setIsActive(false))}
-            }
-        })
-    }, [])
 
     return (
         <>
